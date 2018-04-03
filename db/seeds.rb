bdf00bd3f81252a5a985c1d33fa64b2c65d9d94a
KingDavid2 <nausito@gmail.com>
2018-04-03 16:04:02 -0500

    adds seed /test.json

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do
+  Test.create name: Faker::Name.name, tel: Faker::PhoneNumber, data: {pokemon: Faker::Pokemon, lenguage: Faker::ProgrammingLanguage}
+end
